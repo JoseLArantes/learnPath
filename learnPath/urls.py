0e8e437c16404d7e9dc3a8f575a476ecbe77c619
Jose Arantes <jlarantes@gmail.com>
2024-09-12 19:11:03 -0300

    Remove template frontend - Use API and Admin only

--- learnPath/urls.py
+++ learnPath/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 schema_view = get_schema_view(
@@ -26,6 +25,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-
-    path('', TemplateView.as_view(template_name='index.html')),
 ]
